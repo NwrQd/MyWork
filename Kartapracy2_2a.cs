6f20b15db63b47a80dca7fa6ac83c6b25c1d3919
NwrQd <pspaikovich@gmail.com>
2025-09-15 15:47:56 +0200

    Add files via upload

---
+++ Kartapracy2_2a.cs
@@ -0,0 +1,134 @@
+﻿//zadanie 1
+//int a = int.Parse(Console.ReadLine());
+//if (a % 3 == 0)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+
+
+//zadanie2
+//int a = int.Parse(Console.ReadLine());
+//if (a >= 100 || a < 999 && a % 17 == 0)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+
+//zadanie3
+//int a = int.Parse(Console.ReadLine());
+//if (a < 18)
+//    Console.WriteLine("Jesteś pełnoletni");
+//else
+//    Console.WriteLine("Jesteś nie pełnoletni");
+
+
+//zadanie4
+//int a = int.Parse(Console.ReadLine());
+//if (a < 20)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+
+
+//zadanie5
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//if (c < b || c > a)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+
+//zadanie 6
+//using System.ComponentModel.Design;
+
+//int a = int.Parse(Console.ReadLine());
+//int p = int.Parse(Console.ReadLine());
+//if ((Math.Pow(a, p) - a % p == 0))
+//    Console.WriteLine("Tak");
+//else 
+//    Console.WriteLine("Nie");
+
+
+//zadanie7
+//int p = int.Parse(Console.ReadLine());
+//int k = int.Parse(Console.ReadLine());
+//int s = int.Parse(Console.ReadLine());
+//if (3 * s >= (k - p))
+//    Console.WriteLine("TAK");
+//else
+//Console.WriteLine("NIE");
+
+//KARTA PRACY2a
+//zadanie1
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int suma = a + b;
+//if (suma % 2 == 0)
+//    Console.WriteLine("TAK");
+//else
+//Console.WriteLine("NIE");
+
+//zadanie 2
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//if ((a + b) / 2 > Math.Sqrt(a*b))
+//    Console.WriteLine("Arytmetyczna jest większa");
+//else
+//    Console.WriteLine("Geometryczna jest większa");
+
+
+//zadanie3
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//if (a == b && a != c)
+//    Console.WriteLine("TAK:"+ a);
+//else if (b == c && c != a)
+//    Console.WriteLine("TAK:"+ b);
+//else if (c == a && c != b) 
+//    Console.WriteLine("TAK:"+ c);
+
+//else Console.WriteLine("NIE")
+
+//zadanie4
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//int d = int.Parse(Console.ReadLine());
+//if (a <= b && a <= c && a <= d)
+//    Console.WriteLine(a);
+
+//else if (b <= a && b <= c && b <= d)
+//    Console.WriteLine(b);
+
+//else if (c <= b && c <= a && c <= d)
+//    Console.WriteLine(c);
+
+//else if (d <= a && d <= c && d <= b)
+//    Console.WriteLine(d);
+
+//else
+//    Console.WriteLine("Błąd, podałeś kilka jednakowych liczb");
+
+//zadanie5
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//if (a + b > c && a + c > b && b + c > a) 
+//    Console.WriteLine("Kąt jest równy");
+
+//else Console.WriteLine("Kąt nie jest równy");
+
+//zadanie6
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//if (a*a + b*b > c*c) 
+//    Console.WriteLine("Kąt jest ostrokątny");
+
+//else if (a*a + b*b = c*c)
+//    Console.WriteLine("Kąt jest prostokątny");
+
+//else if (a*a + b*b < c*c)
+//    Console.WriteLine("Kąt jest rozwartokątny");
+
