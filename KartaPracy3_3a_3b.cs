e6dd76fb9258abe2142d2c07696131d2a1bcde18
NwrQd <pspaikovich@gmail.com>
2025-09-15 15:56:09 +0200

    Add files via upload

---
+++ KartaPracy3_3a_3b.cs
@@ -0,0 +1,25 @@
+﻿//zadanie1
+//for (int i = 13; i < 30; i++)
+//    Console.Write(i + " ");
+
+//zadanie2
+//for (int i = 1; i < 10; i = i + 3)
+//    Console.Write(i + " ");
+
+//zadanie3
+//for (int i = 1000; i < 10000; i++)
+//    if (i % 379 == 0)
+//    Console.Write(i + " ");
+
+//zadanie4
+
+//zadanie5
+//int n  = int.Parse(Console.ReadLine());
+//int x;
+//int w = 0;
+//for (int i = 1; i <= n; i++)
+//{
+//    x = int.Parse(Console.ReadLine());
+//    w = w + x;
+//}
+//Console.WriteLine(w);
